75c24140947df7e4bb669cef44d773ac67e5aa74
Rafa-Lino <rafaelino71@gmail.com>
2023-02-16 08:50:59 -0300

    Exercicio While

---
+++ Estrutura_de_Dados/Aula2_While/src/main/java/Aula2_While/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Aula2_While;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int i = 0;
+        
+        while(i <= 10) {
+            System.out.println(i);
+            i += 2;
+        }
+        
+    }
+    
+}

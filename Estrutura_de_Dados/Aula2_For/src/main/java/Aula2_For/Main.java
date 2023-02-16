252d25a5d4199a5a9702ca637b92f49c62a701cd
Rafa-Lino <rafaelino71@gmail.com>
2023-02-16 11:05:33 -0300

    Exercicio For

---
+++ Estrutura_de_Dados/Aula2_For/src/main/java/Aula2_For/Main.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Aula2_For;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         
+         int fatorial = 10;
+         
+         for(int i = fatorial - 1; i > 1; i--) {
+             
+             fatorial = fatorial * i;
+             
+             System.out.println(fatorial);
+                               
+            
+         }
+        
+         
+        
+    }
+    
+}
